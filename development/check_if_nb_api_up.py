342ee112c0a2cea79464ebf4813c703d0b36d6e5
Jamie Murphy <jamiemurphyit@gmail.com>
2022-08-22 21:37:26 +0100

    Update check_if_nb_api_up.py

--- development/check_if_nb_api_up.py
+++ development/check_if_nb_api_up.py
@@ -6,13 +6,14 @@ import os
 
 nb_not_up = True
 api_url = "http://netbox:8000/api"
-max_timeout_attempts = 35
+max_timeout_attempts = 20
 current_attempts = 0
 while nb_not_up:
     try:
         response = requests.get(api_url)
-        if "circuits" in response.json():
+        if "circuits" in response.text:
             nb_not_up = False
+            exit(0)
     except:
         print("did not get a repsonse yet")
     if not current_attempts < max_timeout_attempts:
